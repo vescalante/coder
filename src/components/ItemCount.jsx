5200378649ffdece02d97a401a54b99acde9977e
Victor Escalante <vicx74ever@gmail.com>
2022-06-01 20:14:33 -0500

    Adding a Counter for items limited by 5 pz in stock

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import "./itemcount.css";
+
+function ItemCount({stock, initial, onAdd}) {
+    const [InitialVal, setInitialVal]=useState(initial);
+    const [StockVal, setStockVal]=useState(stock);
+    const [disablePlus,setDisablePlus]=useState(false);
+    const [disableAdd,setDisableAdd]=useState(false);
+
+    useEffect(()=>{
+        if(InitialVal>=StockVal){
+            alert('Te has llegado al limite del stock');
+            setDisablePlus(true);
+            setDisableAdd(true);
+        }else if(InitialVal<=0){
+            setDisableAdd(true);
+        }else{
+            setDisableAdd(false);
+            setDisablePlus(false);
+        }
+    },[InitialVal]);
+
+  return (
+    <div className='row'>
+        <div className='col-lg-4'>
+            <div className="card">
+                <div className="card-body">
+                    <div className="product-form container">
+                        <div className="product-qty-form">
+                            <div className="input-group">
+                                <button className="button-minus"
+                                    onClick={()=>{
+                                        if(InitialVal>=1){
+                                            setInitialVal(InitialVal-1);
+                                        }
+                                    }}>-</button>
+                                <input type="number" value={InitialVal} name="quantity" className="quantity-field" readOnly='readOnly' />
+                                <button className="button-plus"
+                                    onClick={()=>{
+                                        setInitialVal(InitialVal+1);
+                                    }}
+                                    disabled={disablePlus}>+</button>
+                            </div>
+                        </div>
+                        <button className="btn btn-primary btn-cart" onClick={()=> onAdd(InitialVal)} disabled={disableAdd}>
+                            <span>Agregar al carrito</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import ItemCount from './ItemCount';
 import "./itemlistcontainer.css";
 
 function ItemListContainer({msg, nombre}) {
   return (
-    <div className='container-fluid'>
-        <div className='row'>
-            <div className='col-lg-12 p-5 title-saludo'>
+    <div className='container'>
+        <div className='row pt-5 pb-2'>
+            <div className='col-lg-12 title-saludo'>
                 <h3>Hola <span>{nombre}</span>, {msg}</h3>
             </div>
         </div>
+        <ItemCount 
+          stock={5} 
+          initial={1} 
+          onAdd={ function onAdd(e){
+            alert("El valor del carrito es:"+ e);
+          }}
+        />
     </div>
   )
 }
